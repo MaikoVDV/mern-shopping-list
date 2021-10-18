6b04d0ab5e961152f4db09b88dd60a3a75ce2025
Pancake Protector <maikovdveen@gmail.com>
2021-10-18 17:07:37 +0200

    fixed everything

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { Provider } from 'react-redux';
+import store from './store.js';
+
+import { Container } from 'reactstrap'
+
+import AppNavbar from "./components/AppNavBar.js";
+import ShoppingList from "./components/ShoppingList.js";
+import ItemModal from './components/ItemModal.js';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <AppNavbar />
+        <Container>
+          <ItemModal />
+          <ShoppingList />
+        </Container>
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = { 
+    mongoURI: "mongodb+srv://Administrator:SuperSecureAdminPassword@cluster0.jw1fn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}
